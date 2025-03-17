9ba518fe39a334e0b63b7c2b7dd2c33bea77a3fb
ParadigmMango2 <jacobjhartt@gmail.com>
2025-03-17 13:28:47 -0600

    FIX: host typo

--- active_interview_project/active_interview_project/settings.py
+++ active_interview_project/active_interview_project/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['app-cs4300craiglillemon-5.devedu.io',
                  'localhost', '127.0.0.1', 'localhost:3000', '127.0.0.1:3000',
-                 '172.17.0.2', 'djano']
+                 '172.17.0.2', 'django']
 
 # Application definition
 
