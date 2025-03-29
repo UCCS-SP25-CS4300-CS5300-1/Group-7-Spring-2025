1178962c1b54afc939b2fcfcc102030a330aea0d
ParadigmMango2 <jacobjhartt@gmail.com>
2025-03-29 12:41:30 -0600

    implement chat model

--- active_interview_backend/active_interview_app/models.py
+++ active_interview_backend/active_interview_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Chat(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    record = models.TextField() # Json string of the messages object
+    # TODO: mandatory foreignkey to job listing object
+    # TODO: optional foreignkey to resume object
