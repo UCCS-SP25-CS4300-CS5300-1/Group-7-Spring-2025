14d98609dd54b6c62519f1c06b79653eacd5e527
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-03 16:16:56 -0600

    Move chrome to dockerfile

--- active_interview_backend/active_interview_app/tests/test_e2e.py
+++ active_interview_backend/active_interview_app/tests/test_e2e.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -12,11 +14,15 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class TestHost(LiveServerTestCase):
     def test0010(self):
+        # make temp profile dir
+        temp_profile_dir = tempfile.mkdtemp()
+
         # Configure chrome options
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
         
         # Init chrome driver
         driver = webdriver.Chrome(options=chrome_options)
