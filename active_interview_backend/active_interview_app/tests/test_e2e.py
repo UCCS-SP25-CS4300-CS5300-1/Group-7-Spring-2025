a0cd19448028b37e3e93f1961bfa6afa7b84701c
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-03 14:38:13 -0600

    Update server test case to be headless

--- active_interview_backend/active_interview_app/tests/test_e2e.py
+++ active_interview_backend/active_interview_app/tests/test_e2e.py
@@ -4,11 +4,21 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Hosttest(LiveServerTestCase):
+class TestHost(LiveServerTestCase):
     def test0010(self):
-        driver = webdriver.Chrome()
+        # Configure chrome options
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+        
+        # Init chrome driver
+        driver = webdriver.Chrome(options=chrome_options)
+
+        driver.quit()
