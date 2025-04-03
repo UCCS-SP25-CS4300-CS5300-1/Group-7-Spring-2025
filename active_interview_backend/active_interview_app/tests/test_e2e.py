42c87d84dcbfa4627d4ca089d23800c3e41df701
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-03 14:19:56 -0600

    prepare initial files for selenium testing

---
+++ active_interview_backend/active_interview_app/tests/test_e2e.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
