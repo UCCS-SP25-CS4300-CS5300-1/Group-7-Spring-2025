e5c027ba268124651935fe7e356ea6811de5f124
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-04 15:25:59 -0600

    Run CI without testing user

--- active_interview_backend/active_interview_app/tests/test_e2e.py
+++ active_interview_backend/active_interview_app/tests/test_e2e.py
@@ -7,7 +7,6 @@ from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -25,10 +24,9 @@ class TestHost(LiveServerTestCase):
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
+        # chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
         
         # Init chrome driver
-        service = Service("/usr/local/bin/chromedriver")
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
 
         driver.quit()
