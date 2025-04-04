94fae141ff4dfa6d54ba3e6133772e29746008a5
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-04 12:24:11 -0600

    Update chrome options

--- active_interview_backend/active_interview_app/tests/test_e2e.py
+++ active_interview_backend/active_interview_app/tests/test_e2e.py
@@ -19,10 +19,12 @@ class TestHost(LiveServerTestCase):
 
         # Configure chrome options
         chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
+        # chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
         
         # Init chrome driver
         driver = webdriver.Chrome(options=chrome_options)
