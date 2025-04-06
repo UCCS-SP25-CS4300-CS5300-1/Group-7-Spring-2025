a09c50c89ca6b8b33483199b553d8eddd6d40a91
ParadigmMango2 <jacobjhartt@gmail.com>
2025-04-06 12:47:30 -0600

    Account links implemented and other refactors

--- active_interview_backend/active_interview_app/urls.py
+++ active_interview_backend/active_interview_app/urls.py
@@ -17,22 +17,23 @@ router = routers.DefaultRouter()
 
 
 urlpatterns = [
+    # Misc. urls
     path('', views.index, name='index'),
-    path('', views.index, name='login'),
+
+    # Auth urls
     path('testlogged/', views.loggedin, name='loggedin'),
     path('accounts/', include('django.contrib.auth.urls')),
-
-    path('accounts/register', views.register, name='register_page'),
+    path('accounts/register/', views.register, name='register_page'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 
-    # Chat views
+    # Chat urls
     path('chat/', views.chat_list, name='chat-list'),
     path('chat/create/', views.CreateChat.as_view(), name='chat-create'),
     path('chat/<int:chat_id>/', views.ChatView.as_view(), name='chat-view'),
     path('chat/<int:chat_id>/edit/', views.EditChat.as_view(), name='chat-edit'),
     path('chat/<int:chat_id>/delete/', views.DeleteChat.as_view(), name='chat-delete'),
 
-    # Demo view
+    # Demo urls
     # path('demo/', views.demo, name='demo'),
     # path('chat-test/', views.test_chat_view, name='chat-test'),
 
