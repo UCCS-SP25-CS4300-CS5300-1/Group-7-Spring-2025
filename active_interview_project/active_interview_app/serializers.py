1ae6d75486d1515d61a3917381b879bd4c9b4b16
Jwillis95 <jiwillis95@gmail.com>
2025-04-07 15:42:19 -0600

    Update serializers.py
    Uploaded serializers.py.

---
+++ active_interview_project/active_interview_app/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import UploadedFile, PastedText
+
+class UploadedFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadedFile
+        fields = ['id', 'file', 'user', 'uploaded_at']
+
+class PastedTextSerializer(serializers.Serializer):
+    text = serializers.CharField()
+    class Meta:
+        model = PastedText
+        fields = ['id', 'user', 'filename', 'content', 'created_at']
