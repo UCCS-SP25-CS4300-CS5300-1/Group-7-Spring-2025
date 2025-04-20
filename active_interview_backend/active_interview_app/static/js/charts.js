aa292f6f3dffb4b2aa5a75a619f750afcd87cd54
Maddison Ratzlaff <maddie@ratzlaff.us>
2025-04-19 23:31:33 -0600

    started results page

--- active_interview_backend/active_interview_app/static/js/charts.js
+++ active_interview_backend/active_interview_app/static/js/charts.js
@@ -5,9 +5,14 @@ After a user completes their mock interview they will be given a page of their r
 //Bar Graph
 const xValues = ["Coherence", "Clarity", "Correctness", "Politeness"];
 const yValues = [55, 49, 44, 50];
-const barColors = ["red", "green","blue","orange"];
+const barColors = ["#4482A6", "#F5F9E9","#96A13A","#564256"];
 
-const BarChart = new Chart("BarChart", {
+Chart.defaults.backgroundColor = '#9BD0F5';
+Chart.defaults.borderColor = '#36A2EB';
+Chart.defaults.color = '#000';
+
+const BarChart = document.getElementById('BarChart').getContext('2d');
+const chart1 = new Chart(BarChart, {
     type: "bar",
     data: {
       labels: xValues,
@@ -17,9 +22,44 @@ const BarChart = new Chart("BarChart", {
       }]
     },
     options: {
+        font: {
+          family: "Verdana, Geneva, Tahoma, sans-serif",
+          size: 15,
+          color: "blue",
+        },
         title: {
           display: true,
           text: "Category scores (out of 100)",
-        }
-    }
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+    },
   });
+
+const DonutChart = document.getElementById("DonutChart").getContext('2d');
+const chart2 = new Chart(DonutChart, {
+  type: 'doughnut',
+
+  data: {
+    labels: [
+      'Professionalism',
+      'Speed',
+      'Grammar'
+    ],
+    datasets: [{
+      data: [200, 50, 100],
+      backgroundColor: barColors,
+      hoverOffset: 4
+    }],
+  },
+  options: {
+    title:{
+      display: true,
+      text: 'Categories',
+    },
+  },
+});
+
